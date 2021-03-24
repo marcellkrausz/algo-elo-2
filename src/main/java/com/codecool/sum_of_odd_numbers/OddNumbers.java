52941fa11fde78475e2ea108fcd8043913e2bbb6
marcellkrausz <krauszmaarcell@gmail.com>
2021-03-24 16:47:54 +0100

    add sum

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -6,13 +6,11 @@ public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
         Integer sum = 0;
-        Integer countOdd = 0;
         for (Integer number : numbers) {
             if (number % 2 != 0) {
                 sum += number;
-                countOdd++;
             }
         }
-        return countOdd == 0 ? 0 : sum;
+        return sum;
     }
 }
