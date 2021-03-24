d8326cc3837b712f380071a68240e3a46815121d
marcellkrausz <krauszmaarcell@gmail.com>
2021-03-24 16:39:25 +0100

    add class

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -7,10 +7,10 @@ public class OddNumbers {
     public static Integer sum(List<Integer> numbers) {
         Integer sum = 0;
         Integer countOdd = 0;
-        for(Integer number : numbers) {
-            if(number % 2 != 0) {
-              sum += number;
-              countOdd++;
+        for (Integer number : numbers) {
+            if (number % 2 != 0) {
+                sum += number;
+                countOdd++;
             }
         }
         return countOdd == 0 ? 0 : sum;
